88117c5a845cb0228c8c695166b1bf8f7f7f1038
Catherine Almira <almiratjann@gmail.com>
2017-11-28 10:21:48 +0700

    initialize and nexthop function

--- src/DVP.java
+++ src/DVP.java
@@ -28,22 +28,44 @@ public class DVP {
 		for(int i=1; i<=size; i++){
 			for(int j=1; j<=size; j++) {
 				dist[i][j] = UNDEFINED;
-				if (i == j)
+				if (i == j) {
 					dist[i][j] = 0;
-				nexthop[i][j] = i; // TODO: nexthop masi blm diinisialisasi dgn benar
+					// Inisialisasi nexthop
+					nexthop[i][j] = j;
+				} else {
+					nexthop[i][j] = -1;
+				}
+				//nexthop[i][j] = i; // TODO: nexthop masi blm diinisialisasi dgn benar
 			}
 		}
 	}
 
 	public void initialize(int row, int[] neighbours) {
-		for(int i=0; i<neighbours.length; i++) {
-			dist[row][neighbours[i]] = 1;
-			// TODO: inisialisasi nexthop
-		}
+		// for(int i=0; i<neighbours.length; i++) {
+		// 	dist[row][neighbours[i]] = 1;
+		// 	dist[neighbours[i]][row] = 1;
+		// 	// TODO: inisialisasi nexthop
+		// }
+		int node_x = neighbours[0];
+		int node_y = neighbours[1];
+		dist[node_x][node_y] = 1;
+		dist[node_y][node_x] = 1;
+		// Inisialisasi nexthop
+		nexthop[node_x][node_y] = node_y;
+		nexthop[node_y][node_x] = node_x;		
 	}
 
 	public void update(int src, int dest) {
 		// klo ini kelar, tubes kelar
+		for (int i = 1; i <= size; i++) {
+			if (dist[src][i] != UNDEFINED && i != dest) {
+				if (dist[dest][i] == UNDEFINED) {
+					dist[dest][i] += 1;
+				}
+				dist[dest][i] += dist[src][i];
+				nexthop[dest][i] = src;
+			}
+		}
 	}
 
 	public String toString() {
